1ca3aed9c76277d24d2a8f7b05bbb956a55b8a5d
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-08-26 10:58:32 +0200

    Add ront-end part of sign in and sign up functionnality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import {Button} from "@/components/ui/button";
+import {AuthScreen} from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Coucou ! 
-      <Button variant="slack">Cliquez-moi !</Button>
+    <div >
+      <AuthScreen />
     </div>
   );
 }
